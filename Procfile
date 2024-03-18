29e027ab174df4a20a9d610e79011368a4d41f51
GhGuda <toyboipressure1@gmail.com>
2024-03-18 01:08:09 +0000

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn averyproject.wsgi --log-file -
---
+++ app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GctradeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gctrade'
